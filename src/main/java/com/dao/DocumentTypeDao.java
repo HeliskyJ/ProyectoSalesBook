4fe854dbd6d9e7f47a7958c40b5960e0218e63a8
Hely <skljhly.666@gmail.com>
2019-03-09 18:54:57 -0600

    Añade lista DocumentTypeDao

--- src/main/java/com/dao/DocumentTypeDao.java
+++ src/main/java/com/dao/DocumentTypeDao.java
@@ -9,7 +9,10 @@ import com.conexion.Conexion;
 import com.models.DocumentType;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -35,5 +38,31 @@ public class DocumentTypeDao {
            System.out.println("Error de insert "+e);
            return false;
        }
+    }
+    public static List<DocumentType> tipoDoc(){
+        List<DocumentType> list = new ArrayList<>();
+        Connection con;
+        PreparedStatement ps;
+        ResultSet rs;
+        String sql = "Select *from tipo_telefono";
+        
+        try{
+            con=Conexion.getConexion();
+            ps=con.prepareStatement(sql);
+            rs=ps.executeQuery();
+            
+            while(rs.next()){
+                list.add(new DocumentType(
+                        rs.getInt("id"),
+                        rs.getString("document_name")
+                ));
+            }
+            ps.close();
+            con.close();
+        }catch(SQLException e){
+            System.out.println("Error select DocType "+ e.getMessage());
+            list.clear();
+        }
+        return list;
     }
 }
